c4c94e3f3051d57e93515e1966cd063d97f55ccf
yavuzsonmez <contact@yavuzsonmez.com>
2021-10-28 12:44:48 +0200

    update

--- script.sh
+++ script.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+#### Update the whole shit
 sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
+
+#### Install pentesting tools from a list
 sudo apt install $(cat tools.list | tr "\n" " ") -y
+
+#### Privilege escalation tools suite
+git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git
+
+#### Make a backup of the .bashrc file
+cp ~/.bashrc ~/.bashrc.bak
+
+#### Customize bash prompt
+echo 'export PS1="-[\[$(tput sgr0)\]\[\033[38;5;10m\]\d\[$(tput sgr0)\]-\[$(tput sgr0)\]\[\033[38;5;10m\]\t\[$(tput sgr0)\]]-[\[$(tput sgr0)\]\[\033[38;5;214m\]\u\[$(tput sgr0)\]@\[$(tput sgr0)\]\[\033[38;5;196m\]\h\[$(tput sgr0)\]]-\n-[\[$(tput sgr0)\]\[\033[38;5;33m\]\w\[$(tput sgr0)\]]\\$ \[$(tput sgr0)\]"' >> ~/.bashrc
